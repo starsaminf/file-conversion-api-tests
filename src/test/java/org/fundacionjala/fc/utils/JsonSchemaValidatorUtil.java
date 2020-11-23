b566fbb69218ae44dbe2d2ebb941eba7416dc0fd
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-11-22 20:07:29 -0400

    An initial version to a method that validate a response from a Json Schema File

--- src/test/java/org/fundacionjala/fc/utils/JsonSchemaValidatorUtil.java
+++ src/test/java/org/fundacionjala/fc/utils/JsonSchemaValidatorUtil.java
@@ -1,6 +1,14 @@
 package org.fundacionjala.fc.utils;
 
+import io.restassured.response.Response;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+
+import java.io.File;
+
 public final class JsonSchemaValidatorUtil {
 
-
+    public static void compareResponseVsJsonSchema(Response response, String schemaFilePath) {
+        File schemaFile = new File(schemaFilePath);
+        response.then().assertThat().body(matchesJsonSchema(schemaFile));
+    }
 }
