7ada8f3093b10cd4d419f09ff7ceb88b64696117
Mirko Romay Ramos <mirko.romay@fundacion-jala.org>
2020-11-24 13:00:28 -0400

    Adding javadoc comments and correcting CheckStyle issues

--- src/test/java/org/fundacionjala/fc/utils/JsonSchemaValidator.java
+++ src/test/java/org/fundacionjala/fc/utils/JsonSchemaValidator.java
@@ -7,7 +7,18 @@ import java.io.File;
 
 public final class JsonSchemaValidator {
 
-    public static void validate(Response response, String schemaFilePath) {
+    /**
+     * This avoids a default constructor for utility class.
+     */
+    private JsonSchemaValidator() {
+    }
+
+    /**
+     * A method that validate a http response from a given JsonSchema file.
+     * @param response the response obtained from the server.
+     * @param schemaFilePath the path of the JsonSchema file.
+     */
+    public static void validate(final Response response, final String schemaFilePath) {
         File schemaFile = new File(schemaFilePath);
         System.out.println(response.getBody());
         response.then().assertThat().body(matchesJsonSchema(schemaFile));
