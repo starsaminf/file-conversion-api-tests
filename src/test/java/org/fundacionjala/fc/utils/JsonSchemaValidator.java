e304b7a417e8c7ce451e1f1fe89b8e8cf8c5b5bc
MirkoFer-622 <61889294+MirkoFer-622@users.noreply.github.com>
2020-11-24 16:28:10 -0400

    [develop]  JSON Schema Validation Class (#14)
    * Adding a new dependency for json-schema-validator and create an utilitarian class that use the new dependency
    
    * An initial version to a method that validate a response from a Json Schema File
    
    * updating name of JsonSchemaValidator class and validate() method and using json-schema-validator version 4.3.2 instead 4.3.0
    
    * Adding javadoc comments and correcting CheckStyle issues
    
    * Deleting unnecesary print line at vaidate() method

---
+++ src/test/java/org/fundacionjala/fc/utils/JsonSchemaValidator.java
@@ -0,0 +1,25 @@
+package org.fundacionjala.fc.utils;
+
+import io.restassured.response.Response;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
+
+import java.io.File;
+
+public final class JsonSchemaValidator {
+
+    /**
+     * This avoids a default constructor for utility class.
+     */
+    private JsonSchemaValidator() {
+    }
+
+    /**
+     * A method that validate a http response from a given JsonSchema file.
+     * @param response the response obtained from the server.
+     * @param schemaFilePath the path of the JsonSchema file.
+     */
+    public static void validate(final Response response, final String schemaFilePath) {
+        File schemaFile = new File(schemaFilePath);
+        response.then().assertThat().body(matchesJsonSchema(schemaFile));
+    }
+}
