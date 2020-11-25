0c3e5cf4c5f4eddcadea4c3a3237aa017fc36af2
roht0515 <rodrigo051599@gmail.com>
2020-11-25 17:46:09 -0400

    code review fixes

--- src/main/java/org/fundacionjala/fc/config/PropertiesReader.java
+++ src/main/java/org/fundacionjala/fc/config/PropertiesReader.java
@@ -10,115 +10,81 @@ import java.util.Properties;
  */
 public final class PropertiesReader {
 
-    /**
-     * FileReader attribute.
-     */
-    private static FileReader reader = null;
-
-    /**
-     * Properties attribute.
-     */
-    private static Properties property = null;
-
-    /**
-     * path from the file.properties.
-     */
+    private FileReader reader;
+    private Properties property;
     private static final String PATH = "gradle.properties";
+    private static PropertiesReader singleInstance;
 
-    /**
-     * PropertiesReader attribute.
-     */
-    private static PropertiesReader singleInstance = null;
     private PropertiesReader() {
+        try {
+            reader = new FileReader(PATH);
+            property = new Properties();
+            property.load(reader);
 
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * get instance or create a new one.
      * @return PropertiesReader instance.
      */
-    public static PropertiesReader getInstance() throws IOException {
+    public static PropertiesReader getInstance() {
         if (singleInstance == null) {
-            setReader();
-            setPropertiesReader();
-            initializeClass();
+            singleInstance = new PropertiesReader();
         }
         return singleInstance;
-    }
-
-    /**
-     * instance the reader class.
-     */
-    private static void setReader() throws FileNotFoundException {
-        reader = new FileReader(PATH);
-    }
-
-    /**
-     * instance the properties class.
-     */
-    private static void setPropertiesReader() throws IOException {
-        property = new Properties();
-        property.load(reader);
-    }
-
-    /**
-     * instance the PropertiesReader class.
-     */
-    private static void initializeClass() {
-        singleInstance = new PropertiesReader();
     }
 
     /**
      * get the BaseUrl from the file.properties.
      * @return base url.
      */
-    public static String getBaseUrl() {
-        String baseUrl = property.getProperty("baseUrl");
-        return baseUrl;
+    public String getBaseUrl() {
+        return property.getProperty("baseUrl");
     }
 
     /**
      * get the User from the file.properties.
      * @return User value.
      */
-    public static String getUser() {
-        String user = property.getProperty("user");
-        return user;
+    public String getUser() {
+        return property.getProperty("user");
     }
 
     /**
      * get the password from the file.properties.
      * @return Password value.
      */
-    public static String getPassword() {
-        String password = property.getProperty("password");
-        return password;
+    public String getPassword() {
+        return property.getProperty("password");
     }
 
     /**
      * get the audioTemplatesPath from the file.properties.
      * @return AudioTemplatesPath value.
      */
-    public static String getAudioTemplatesPath() {
-        String audioTemplatesPath = property.getProperty("audioTemplatesPath");
-        return audioTemplatesPath;
+    public String getAudioTemplatesPath() {
+        return property.getProperty("audioTemplatesPath");
     }
 
     /**
      * get the videoTemplatesPath from the file.properties.
      * @return videoTemplatesPath value.
      */
-    public static String getVideoTemplatesPath() {
-        String videoTemplatesPath = property.getProperty("videoTemplatesPath");
-        return videoTemplatesPath;
+    public String getVideoTemplatesPath() {
+        return property.getProperty("videoTemplatesPath");
     }
 
     /**
      * get the imageTemplatesPath from the file.properties.
      * @return imageTemplatesPath value.
      */
-    public static String getImageTemplatesPath() {
-        String imageTemplatesPath = property.getProperty("imageTemplatesPath");
-        return imageTemplatesPath;
+    public String getImageTemplatesPath() {
+        return property.getProperty("imageTemplatesPath");
     }
+
 }
